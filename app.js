a103fa12316dcce31183d115711a2669eaf8f7aa
Timur <68633358+TimGon@users.noreply.github.com>
2023-01-14 22:07:32 +0400

    test file

---
+++ app.js
@@ -0,0 +1,27 @@
+// console.log("Hello world! Привет мир!");
+// let arr = [1,2,3];
+// console.table(arr)
+
+// // // array
+// // console.log(process.argv, process.argv.length);
+
+// //process data
+// const count = process.argv.length;
+// console.log('Количество элементов:', count);
+// for(let i = 0; i < count; i++) {
+//     console.log(process.argv[i]);
+// }
+
+// Практика с parseInt
+
+// let x = 10;
+// let y = 15;
+
+// console.log(parseInt('0xFF') );
+
+{
+    let x = 10;
+    var y = 15;
+}
+
+console.log(y)
---
+++ script.bat
@@ -0,0 +1,8 @@
+@echo off
+title Запуск программы app.js
+echo Результат:
+@echo.
+node app.js
+@echo.
+@echo.
+pause
